1af0bc86ea0db6365e0fa33eae34d4998dc5bac6
erutan-lamfornok <branislav.nikol@gmail.com>
2017-03-05 16:35:12 +0100

    Added files

---
+++ main.py
@@ -0,0 +1,74 @@
+import pygame, sys, os
+sys.path.append(os.getcwd())
+from utils import tools
+
+pygame.init()  # initiates all inside pygame; returns a tuple
+
+# loading graphic objects
+img_apple = pygame.image.load('images/apple.png')
+img_head = pygame.image.load('images/snake_head.png')
+
+# defining game dimensions
+up_width = 30
+block_size = img_head.get_width() #20
+grid = [35, 25]  # Here we define the grid(x,y) for the display (e.g. 35x25).
+display_size = grid[0] * block_size, grid[1] * block_size + up_width
+
+# defining colors
+colors = dict(white=(255, 255, 255),
+              black=(0, 0, 0),
+              red=(255, 0, 0),
+              green=(0, 155, 0))
+
+# Defining custom fonts
+myfonts = dict(smallfont=pygame.font.SysFont("comicsansms", 15),
+               medfont=pygame.font.SysFont("comicsansms", 30),
+               bigfont=pygame.font.SysFont("comicsansms", 50))
+
+# Global gameplay variables
+level = 0
+FPS = 5
+
+# initializing window for display
+screen = pygame.display.set_mode(display_size)
+clock = pygame.time.Clock()
+
+gui = True
+while gui:
+    version = tools.game_intro(display_size, colors, myfonts, screen)
+    if version == 'simple':
+        from versions import simple
+        game = simple.gameplay(grid, block_size)
+    elif version == 'slither_class':
+        from versions import slither_class
+        game = slither_class.gameplay(grid, block_size)
+    switch = True
+    while switch:
+        dt = clock.tick(FPS)
+
+        events = pygame.event.get()
+        game.update(events, dt)
+
+        screen.fill(colors['white'])
+        #scoretext = myfonts['medfont'].render("Score {0}".format(game.score),\
+        #                   1, colors['black'])
+        #screen.blit(scoretext, (1, 1))
+        tools.score_menu(game.score, colors, myfonts, display_size, screen)
+        game.draw(img_head, colors['green'], img_apple, screen)
+
+        for event in events:
+            if event.type == pygame.QUIT:
+                switch = False
+                pygame.quit()
+                sys.exit('you exited the game')
+            elif event.type == pygame.mouse.get_focused:
+                continue
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_p:
+                    switch = tools.pause(game.score, colors, myfonts, display_size, screen)
+
+        if game.gameover == True:
+            tools.game_over(game.score, colors, myfonts, display_size, screen)
+            switch = False
+
+        pygame.display.flip()
---
+++ versions/simple.py
@@ -0,0 +1,120 @@
+import pygame, sys
+import random
+from pygame.surface import Surface
+
+DIRECTION_UP = "up"
+DIRECTION_RIGHT = "right"
+DIRECTION_DOWN = "down"
+DIRECTION_LEFT = "left"
+
+
+class Vector2:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+class Snake:
+    def __init__(self, pos:Vector2, tail = None):
+        self.pos = pos
+        self.tail = tail
+
+    def take(self, n: int):
+        if n < 1:
+            return None
+        elif self.tail is None:
+            return Snake(self.pos)
+        else:
+            return Snake(self.pos, self.tail.take(n-1))
+
+    def draw(self, color, screen, size):
+        pygame.draw.rect(screen, color, \
+                        [self.pos.x * size, self.pos.y * size, size-1, size-1])
+        if self.tail is not None:
+            self.tail.draw(color, screen, size)
+
+class gameplay:
+    def __init__(self, max_step: list, blocksize: int):
+        """Initiates the actual gameplay and defines relevant parameters for
+        the game.
+
+        Parameters
+        ----------
+        max_step: list [x,y]
+            defines maximum number of steps in x and y.
+        blocksize: int
+            defines the size of each square block.
+        """
+        self.gameover = False
+        self.score = 0
+        self.speed = 0.25
+        self.cooldown = self.speed
+        self.steps = max_step
+        self.direction = DIRECTION_UP
+        self.snake = Snake(Vector2(self.steps[0] // 2, self.steps[1] // 2))
+        self.blocksize = blocksize
+        self.length = 1
+        self.create_new_food()
+
+    def create_new_food(self):
+        self.food = Vector2(random.randint(0, self.steps[0] - 1), \
+                            random.randint(0, self.steps[1] - 1))
+
+    def update(self, events, dt:float):
+        #check for change of direction
+        for event in events:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    self.direction = DIRECTION_UP
+                elif event.key == pygame.K_RIGHT:
+                    self.direction = DIRECTION_RIGHT
+                elif event.key == pygame.K_DOWN:
+                    self.direction = DIRECTION_DOWN
+                elif event.key == pygame.K_LEFT:
+                    self.direction = DIRECTION_LEFT
+                elif event.key == pygame.K_q:
+                    pygame.quit()
+                    sys.exit('you exited the game')
+
+        self.cooldown = self.cooldown - dt
+        if self.cooldown < 0.0:
+            ## Updating snake position
+            if self.direction == DIRECTION_UP:
+                newPos = Vector2(self.snake.pos.x, self.snake.pos.y - 1)
+                if self.snake.pos.y == 0: #Check if point is at boundary
+                    newPos = Vector2(self.snake.pos.x, self.steps[1] - 1)
+            elif self.direction == DIRECTION_RIGHT:
+                newPos = Vector2(self.snake.pos.x + 1, self.snake.pos.y)
+                if self.snake.pos.x == self.steps[0] - 1:
+                    newPos = Vector2(0, self.snake.pos.y)
+            elif self.direction == DIRECTION_DOWN:
+                newPos = Vector2(self.snake.pos.x, self.snake.pos.y + 1)
+                if self.snake.pos.y == self.steps[1] - 1:
+                    newPos = Vector2(self.snake.pos.x, 0)
+            elif self.direction == DIRECTION_LEFT:
+                newPos = Vector2(self.snake.pos.x - 1, self.snake.pos.y)
+                if self.snake.pos.x == 0:
+                    newPos = Vector2(self.steps[0] - 1, self.snake.pos.y)
+            else:
+                raise Exception("direction not supported")
+
+            self.snake = Snake(newPos, self.snake)
+            self.snake = self.snake.take(self.length)
+            self.cooldown += self.speed
+
+            # check if he gets the food
+            if self.snake.pos.x == self.food.x and \
+               self.snake.pos.y == self.food.y:
+                self.snake = Snake(self.snake.pos, self.snake)
+                self.length += 1
+                self.score += 1
+                self.create_new_food()
+
+    def draw(self, snake_head, snake_color, apple_img, screen: Surface):
+        # draws snake
+        self.snake.draw(snake_color, screen, self.blocksize)
+
+        # draws an apple
+        pygame.draw.rect(screen, (255, 0, 0),\
+                         [self.food.x * self.blocksize,\
+                          self.food.y * self.blocksize,\
+                          self.blocksize - 1, self.blocksize - 1])
